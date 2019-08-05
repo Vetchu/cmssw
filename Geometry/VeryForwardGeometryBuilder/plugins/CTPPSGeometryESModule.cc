91ed39dc24ba486a47ce6abe3c9c0e349337a2ed
Jacek Kusnierz <jacek.andrzej.kusnierz@cern.ch>
2019-08-05 17:29:57 +0200

    changed the mapping

--- Geometry/VeryForwardGeometryBuilder/plugins/CTPPSGeometryESModule.cc
+++ Geometry/VeryForwardGeometryBuilder/plugins/CTPPSGeometryESModule.cc
@@ -271,10 +271,11 @@ CTPPSGeometryESModule::buildDetGeomDesc( DDFilteredView* fv, DetGeomDesc* gd )
     else if ( name == DDD_CTPPS_DIAMONDS_SEGMENT_NAME || name == DDD_CTPPS_UFSD_SEGMENT_NAME ) {
       const std::vector<int>& copy_num = fv->copyNumbers();
 
+      std::cout<<"s"<<copy_num<<std::endl;
       const unsigned int id = copy_num[copy_num.size()-1];
       const unsigned int arm = copy_num[1]-1;
-      const unsigned int station = 1;
-      const unsigned int rp = 6;
+      const unsigned int station = copy_num[3]%100/10;
+      const unsigned int rp = copy_num[3]%10;
       const unsigned int plane = ( id / 100 );
       const unsigned int channel = id % 100;
 
@@ -283,16 +284,17 @@ CTPPSGeometryESModule::buildDetGeomDesc( DDFilteredView* fv, DetGeomDesc* gd )
 
     // diamond/UFSD RPs
     else if ( name == DDD_CTPPS_DIAMONDS_RP_NAME ) {
-      const std::vector<int>& copy_num = fv->copyNumbers();
-
-      // check size of copy numubers array
+	const std::vector<int>& copy_num = fv->copyNumbers();
+	
+      std::cout<<"d"<<copy_num<<std::endl;
+      // check size of copy numbers array
       if ( copy_num.size() < 2 )
         throw cms::Exception("DDDTotemRPContruction") << "size of copyNumbers for diamond RP is "
           << copy_num.size() << ". It must be >= 2.";
 
       const unsigned int arm = copy_num[1] - 1;
-      const unsigned int station = 1;
-      const unsigned int rp = 6;
+      const unsigned int station = copy_num[3]%100/10;
+      const unsigned int rp = copy_num[3]%10;
 
       newGD->setGeographicalID( CTPPSDiamondDetId( arm, station, rp ) );
     }
